2798a4d57eba4d86de27676b6b79ee28abe9d7fd
100yann <stoyan.d.kolev@gmail.com>
2023-04-08 11:07:55 +0300

    Added a working highlighter that highlights the word to be written and unhighlights the previous one. Must add functionality for when the user goes back one word, since that unsynchronises what is highlighted and what has to be written.

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ customtkinter.set_default_color_theme("green")
 source_text = all_texts[1]
 text_list = source_text.split()
 
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
@@ -18,7 +19,7 @@ class App(customtkinter.CTk):
         self.title('Typing Speed Test')
         self.config(padx=50, pady=100)
         self.textbox = customtkinter.CTkEntry(master=self,
-                                              height=200,
+                                              height=300,
                                               width=400,
                                               )
         self.textbox.place(relx=0.5, rely=0.5, anchor="center")
@@ -29,28 +30,43 @@ class App(customtkinter.CTk):
         self.reading_text.insert("0.0", self.source_text)
         self.reading_text.configure(state='disabled')
         self.reading_text.place(relx=0.5, rely=0.2, anchor="center")
-
-        self.word_to_highlight = 0 
+        self.start_index = 0
+        self.word_to_highlight = 0
+        self.remove_index = 0
         self.highlight_word()
         self.textbox.bind('<space>', lambda event: (self.return_text(), self.highlight_word()))
         self.textbox.bind('<Return>', lambda event: self.correct_words())
         
 
-    def get_word_indicies(self, text, word):
-        indices = []
-        words = text.split()
-        start_index = 0
-        for char in range(len(words)):
-            if words[char].strip(",.") == word:
-                indices.append((start_index, start_index+len(words[char])))
-            start_index += len(words[char]) + 1
-        return indices
+    def get_word_indicies(self, word, func):
+        if func == "add":
+            indices = (self.start_index, self.start_index+len(word))
+            self.start_index += len(word) + 1
+            return indices
+        elif func == "remove":
+            if self.word_to_highlight > 0:
+                indices = (self.remove_index, self.remove_index+len(word))
+                self.remove_index += len(word) + 1
+                return indices
+                
+
+
 
     def highlight_word(self):
-        pos = self.get_word_indicies(source_text, text_list[self.word_to_highlight])
-        if len(pos) > 1: 
-            self.reading_text.tag_add('addHighlight', f"0.{pos[0][0]}", f"0.{pos[0][1]}")
-        self.reading_text.tag_configure('addHighlight', background='yellow')
+        if self.word_to_highlight > 0:
+            previous_word = text_list[self.word_to_highlight - 1]
+            try:
+                previous_word_pos = self.get_word_indicies(previous_word, "remove")
+                self.reading_text.tag_remove('highlightline', f"1.{previous_word_pos[0]}", f"1.{previous_word_pos[1]}")
+            except _tkinter.TclError:
+                pass
+        
+        word = text_list[self.word_to_highlight]
+        word_pos = self.get_word_indicies(word, "add")
+        starting_pos = word_pos[0]
+        ending_pos = word_pos[1]
+        self.reading_text.tag_add('highlightline', f"1.{starting_pos}", f"1.{ending_pos}")
+        self.reading_text.tag_configure('highlightline', background='yellow')
         self.word_to_highlight +=1
  
 
