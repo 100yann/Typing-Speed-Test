199fec84fe86685f3c17f90df9b9d3cb53730f8e
100yann <stoyan.d.kolev@gmail.com>
2023-04-15 12:40:46 +0300

    made the input text disappear

--- app.py
+++ app.py
@@ -22,29 +22,32 @@ class App(customtkinter.CTk):
         self.title('Typing Speed Test')
         self.state('zoomed')
         self.config(padx=50, pady=100)
+        self.option_add('*Text.Font', "consolas 30")
         self.reading_text = tkinter.Text(master=self, 
                                          height=10,
-                                         width=125,
+                                         width=80,
                                          wrap='word',
                                          font=('Ardoise Std', 16)
                                          )
         self.reading_text.insert("0.0", self.source_text)
         self.reading_text.configure(state='disabled', padx=10, pady=10)
         self.reading_text.place(relx=0.5, rely=0.2, anchor="center")
+
         self.start_index = 0
         self.word_to_highlight = 0
-        self.remove_index = 0
+        self.written_word = 0
+        self.correct_words = 0
 
         self.textbox = customtkinter.CTkTextbox(master=self,
                                 height=100,
                                 width=250,
                                 wrap='word',
                                 )
-        self.textbox.focus_set()
         self.textbox.place(relx=0.5, rely=0.5, anchor="center")
-        self.textbox.bind('<space>', lambda event: (self.return_text(), self.highlight_word()))
+        self.textbox.focus_set()
+        self.textbox.bind('<space>', lambda event: self.return_text())
         self.textbox.bind('<Return>', lambda event: self.correct_words())
-        self.highlight_word()
+        self.highlight_word(text_list[0])
 
         self.timer()
 
@@ -56,47 +59,48 @@ class App(customtkinter.CTk):
         return word_indices
         
 
-    def highlight_word(self):
-        user_text = self.return_text()
-        last_index = len(user_text)
-                
-        if last_index > 0:
+    def highlight_word(self, input_word):
+        if self.written_word > 0:
             self.reading_text.tag_delete('highlight', 
-                                         f'1.{words_and_indices[last_index-1][1]}', 
-                                         f'1.{words_and_indices[last_index-1][2]}')
+                                         f'1.{words_and_indices[self.written_word-1][1]}', 
+                                         f'1.{words_and_indices[self.written_word-1][2]}')
 
-        word = text_list[last_index]   
+        word = text_list[self.written_word]   
         word_pos = self.get_word_indicies(word)
         starting_pos, ending_pos = word_pos[0], word_pos[1]
         self.reading_text.tag_add('highlight', f"1.{starting_pos}", f"1.{ending_pos}")
         self.reading_text.tag_configure('highlight', background='yellow')
-        words_and_indices[last_index] = [word, starting_pos, ending_pos]
+        words_and_indices[self.written_word] = [word, starting_pos, ending_pos]
 
-        if last_index > 0 and user_text[last_index-1] != text_list[last_index-1]:
-            starting_pos, ending_pos = words_and_indices[last_index-1][1], words_and_indices[last_index-1][2]
-            self.reading_text.tag_add('incorrect', f"1.{starting_pos}", f"1.{ending_pos}")
-            self.reading_text.tag_configure('incorrect', underline=True, underlinefg='red')
-        
+        # if self.written_word > 0 and input_word != text_list[self.written_word]:
+        #     starting_pos, ending_pos = words_and_indices[self.written_word][1], words_and_indices[self.written_word][2]
+        #     self.reading_text.tag_add('incorrect', f"1.{starting_pos}", f"1.{ending_pos}")
+        #     self.reading_text.tag_configure('incorrect', underline=True, underlinefg='red')
+        if self.written_word > 0 and input_word == text_list[self.written_word-1]:
+            print(input_word, text_list[self.written_word-1])
+            self.correct_words += 1
+
         self.start_index += len(word) + 1
+        self.written_word += 1
+
 
 
     def return_text(self):
-        input_text = list(self.textbox.get('0.0', 'end').split())
-        return input_text
+        get_input = (self.textbox.get('1.0', 'end')).strip()
+        for char in get_input:
+            if char.isalpha():
+                self.textbox.delete('1.0', "end")
+                self.highlight_word(get_input)
+                break
+        
 
-    def correct_words(self):
-        text = self.return_text()
-        correct_words = 0
-        for index, word in enumerate(text):
-            if word == text_list[index]:
-                correct_words +=1
-        return correct_words
+
 
 
     def timer(self):
         global REMAINING_TIME
         REMAINING_TIME -= 1
-        if REMAINING_TIME > 0:
+        if REMAINING_TIME > 50:
             self.after(1000, self.timer)
             self.entry = customtkinter.CTkEntry(master=self,
                                     placeholder_text=REMAINING_TIME,
@@ -105,6 +109,6 @@ class App(customtkinter.CTk):
                                     )
             self.entry.place(relx=0.0, rely=0.0, anchor="nw")
         else:
-            wpm = self.correct_words()
+            wpm = self.correct_words
             print(f'You can write {wpm} words per minute.')
             self.textbox.configure(state='disabled')
